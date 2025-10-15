a49dea0e021fcfa6f55f4b078e3a4d3b79a05f79
Anton <popsof@yandex.ru>
2025-10-15 15:24:56 +0400

    fix(button): incorrect custom styles applied

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,5 +1,40 @@
 import React from "react";
+import clsx from "clsx";
 
-export const Button = () => {
-  return <div>Button</div>;
+import { ButtonProps } from "@/types/buttonTypes";
+
+const variantButtonStyles = {
+  default: "bg-indigo-600 text-white hover:bg-indigo-700",
+  bordered:
+    "border-[2px] border-[rgba(255,255,255,0.329)] border rounded-[10px] text-gray-900 hover:border-[rgba(255,255,255,0.679)]",
+};
+
+const sizesButtonsStyles = {
+  sm: "px-1 py-2 text-sm",
+  md: "px-4 py-2 text-base",
+  lg: "px-6 py-3 text-lg",
+};
+
+export const Button: React.FC<ButtonProps> = ({
+  children,
+  variant,
+  size,
+  type,
+  onClick,
+  disabled,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      type={type}
+      disabled={disabled}
+      className={clsx(
+        "transition font-medium disabled:opacity-50 disabled:cursor-not-allowed",
+        variantButtonStyles[variant],
+        sizesButtonsStyles[size],
+      )}
+    >
+      {children}
+    </button>
+  );
 };
