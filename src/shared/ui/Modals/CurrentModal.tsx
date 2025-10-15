81b845ab9a864b6db766a8466b3ce3512002dbdd
Anton <popsof@yandex.ru>
2025-10-15 20:29:32 +0400

    chore(currentmodal): create current modal

---
+++ src/shared/ui/Modals/CurrentModal.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { SideMenuModal } from './ui/SideMenu'
+
+export const CurrentModal = () => {
+  return (
+    <div className='h-full'><SideMenuModal/></div>
+  )
+}
