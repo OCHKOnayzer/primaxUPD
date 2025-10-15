d987a8fb902e2132957718861f602ebafbb39939
Anton <popsof@yandex.ru>
2025-10-15 15:32:06 +0400

    refactor(types): update button types

--- src/types/buttonTypes.ts
+++ src/types/buttonTypes.ts
@@ -1,12 +1,11 @@
 import { ReactNode } from "react";
 import { ButtonVariant, ButtonSize, ButtonType } from "./enums/buttonEnums";
 
-export interface ButtonItnerface {
+export interface ButtonProps {
   children?: ReactNode;
   variant: ButtonVariant;
   size: ButtonSize;
   type: ButtonType;
   onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
   disabled?: boolean;
-  className?: string;
 }
