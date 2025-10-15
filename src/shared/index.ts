3648d15330a9d5f41f62fe2e915adf0ea3a27b82
Anton <popsof@yandex.ru>
2025-10-15 20:30:47 +0400

    fix(modals): update modal

--- src/shared/index.ts
+++ src/shared/index.ts
@@ -1,3 +1,5 @@
+export { ModalLayout } from "./ui/Modals";
+export { useModal } from "./ui/Modals";
 export { Button } from "./ui/Button";
 export { Layout } from "./ui/Layout";
 export { Input } from "./ui/Input";
