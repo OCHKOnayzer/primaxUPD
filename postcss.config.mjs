e830a63f3f7c781d1ab545a2bdcc8449d89b6b47
Anton <popsof@yandex.ru>
2025-10-15 15:46:07 +0400

    fix(post): update post config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,5 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'custom-gradient': 'linear-gradient(90deg, rgba(67, 67, 243, 1) 0%, rgba(47, 22, 148, 1) 98%)',
-      }
-    }
-  }
 };
 
 export default config;
