7621c9724cdd68b01de81daebcb29032abe0e079
Anton <popsof@yandex.ru>
2025-10-16 18:00:15 +0400

    .

--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -3,16 +3,18 @@
 import React, { FC, PropsWithChildren } from "react";
 import { Header } from "@/widgets";
 import { Footer } from "@/widgets";
-import { ModalLayout } from "../Modals";
+import { ModalLayout } from "@/shared";
+import { useModal } from "@/shared";
 
 export const Layout: FC<PropsWithChildren> = ({ children }) => {
+  const isModalOpen = useModal((state) => state.isOpen);
+
   return (
     <div className="flex min-h-screen flex-col">
       <Header />
-      <ModalLayout/>
-      <main className="flex-1 relative">
-        {children}
-        </main>
+      {isModalOpen && <ModalLayout />}
+
+      <main className="flex-1 relative">{children}</main>
       <Footer />
     </div>
   );
